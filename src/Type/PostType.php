f13931b70f20485bc55379c59dd1495c4ea58fab
BE-Webdesign <cromleyedwin89@gmail.com>
2016-12-06 23:43:34 -0500

    Add in starting descriptions for the Post type.
    Fixes #70.  This adds in the basic descriptions for the Post type and
    its fields.  When using GraphiQL or another GraphQL introspection system
    handy descriptions are now available!

--- src/Type/PostType.php
+++ src/Type/PostType.php
@@ -13,27 +13,90 @@ class PostType extends BaseType {
 			'name' => 'Post',
 			'fields' => function() use ( $types ) {
 				return array(
-					'id'             => $types->id(),
-					'author'         => $types->user(),
-					'date'           => $types->string(),
-					'date_gmt'       => $types->string(),
-					'content'        => $types->string(),
-					'title'          => $types->string(),
-					'excerpt'        => $types->string(),
-					'post_status'    => $types->string(),
-					'comment_status' => $types->string(),
-					'ping_status'    => $types->string(),
-					'slug'           => $types->string(),
-					'to_ping'        => $types->string(),
-					'pinged'         => $types->string(),
-					'modified'       => $types->string(),
-					'modified_gmt'   => $types->string(),
-					'parent'         => $types->id(),
-					'guid'           => $types->string(),
-					'menu_order'     => $types->string(),
-					'type'           => $types->string(),
-					'mime_type'      => $types->string(),
-					'comment_count'  => $types->int(),
+					'id'              => array(
+						'type'        => $types->id(),
+						'description' => esc_html__( 'The id field matches the WP_Post->ID field.', 'wp-graphql' ),
+					),
+					'author'          => array(
+						'type'        => $types->user(),
+						'description' => esc_html__( "The author field will return a queryable User type matching the post's author.", 'wp-graphql' ),
+					),
+					'date'            => array(
+						'type'        => $types->string(),
+						'description' => esc_html__( 'Post publishing date.', 'wp-graphql' ),
+					),
+					'date_gmt'        => array(
+						'type'        => $types->string(),
+						'description' => esc_html__( 'The publishing date set in GMT.', 'wp-graphql' ),
+					),
+					'content'         => array(
+						'type'        => $types->string(),
+						'description' => esc_html__( 'The content of the post. This is currently just the raw content. An amendment to support rendered content needs to be made.', 'wp-graphql' ),
+					),
+					'title'           => array(
+						'type'        => $types->string(),
+						'description' => esc_html__( 'The title of the post. This is currently just the raw title. An amendment to support rendered title needs to be made.', 'wp-graphql' ),
+					),
+					'excerpt'         => array(
+						'type'        => $types->string(),
+						'description' => esc_html__( 'The excerpt of the post. This is currently just the raw excerpt. An amendment to support rendered excerpts needs to be made.', 'wp-graphql' ),
+					),
+					'post_status'     => array(
+						'type'        => $types->string(),
+						'description' => esc_html__( 'The current status of the post. ( published, draft, etc. ) This should be changed to an enum type supporting valid stati.', 'wp-graphql' ),
+					),
+					'comment_status'  => array(
+						'type'        => $types->string(),
+						'description' => esc_html__( 'Whether the comments are open or closed for this particular post. Needs investigating.', 'wp-graphql' ),
+					),
+					'ping_status'     => array(
+						'type'        => $types->string(),
+						'description' => esc_html__( 'Whether the pings are open or closed for this particular post. Needs investigating.', 'wp-graphql' ),
+					),
+					'slug'            => array(
+						'type'        => $types->string(),
+						'description' => esc_html__( 'The uri slug for the post. This is equivalent to the WP_Post->post_name field and the post_name column in the database for the `wp_posts` table.', 'wp-graphql' ),
+					),
+					'to_ping'         => array(
+						'type'        => $types->string(),
+						'description' => esc_html__( 'URLs queued to be pinged.', 'wp-graphql' ),
+					),
+					'pinged'          => array(
+						'type'        => $types->string(),
+						'description' => esc_html__( 'URLs that have been pinged.', 'wp-graphql' ),
+					),
+					'modified'        => array(
+						'type'        => $types->string(),
+						'description' => esc_html__( 'The local modified time for a post. If a post was recently updated the modified field will change to match the corresponding time.', 'wp-graphql' ),
+					),
+					'modified_gmt'    => array(
+						'type'        => $types->string(),
+						'description' => esc_html__( 'The GMT modified time for a post. If a post was recently updated the modified field will change to match the corresponding time in GMT.', 'wp-graphql' ),
+					),
+					'parent'          => array(
+						'type'        => $types->int(),
+						'description' => esc_html__( 'The ID of the corresponding post parent. This is only typically used with hierarchical content types. This field is equivalent to the value of WP_Post->post_parent and the post_parent column in the `wp_posts` database table.', 'wp-graphql' ),
+					),
+					'guid'            => array(
+						'type'        => $types->string(),
+						'description' => esc_html__( 'The global unique identifier for this post. This currently matches the value stored in WP_Post->guid and the guid column in the `wp_posts` database table.', 'wp-graphql' ),
+					),
+					'menu_order'      => array(
+						'type'        => $types->int(),
+						'description' => esc_html__( 'A field used for ordering posts. This is typically used with nav menu items or for special ordering of hierarchical content types.', 'wp-graphql' ),
+					),
+					'type'            => array(
+						'type'        => $types->string(),
+						'description' => esc_html__( 'This field tells what kind of content type the object is. In WordPress different post types are used to denote different types of content. This field is equivalent to the value of WP_Post->post_type and the post_type column in the `wp_posts` database table.', 'wp-graphql' ),
+					),
+					'mime_type'       => array(
+						'type'        => $types->string(),
+						'description' => esc_html__( 'If the post is an attachment or a media file, this field will carry the corresponding MIME type. This field is equivalent to the value of WP_Post->post_mime_type and the post_mime_type column in the `wp_posts` database table.', 'wp-graphql' ),
+					),
+					'comment_count'  => array(
+						'type'        => $types->int(),
+						'description' => esc_html__( 'The number of comments. Even though WP GraphQL denotes this field as an integer, in WordPress this field should be saved as a numeric string for compatability.', 'wp-graphql' ),
+					),
 					'comments' => [
 						'type' => $types->listOf( $types->comment() ),
 						'description' => 'Returns comments for post based on collection args',
@@ -45,6 +108,7 @@ class PostType extends BaseType {
 					],
 				);
 			},
+			'description' => esc_html__( 'The post object for WordPress. Internally this is an instance of WP_Post. Posts are the main default content type of WordPress. They are the heart and soul of a blog.', 'wp-graphql' ),
 			'interfaces' => [
 				$types->node(),
 			],
